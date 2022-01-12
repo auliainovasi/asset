794fd4e1e2716d8927279aff65ee1ef4d2403568
Aulia Rafi <62741404+rafi273@users.noreply.github.com>
2022-01-12 12:10:49 +0700

    add Content-Length

--- src/controllers/home.ts
+++ src/controllers/home.ts
@@ -3,7 +3,10 @@ import { getFile } from "../models/storage";
 
 export default (req: Request, res: Response) => {
     getFile(req.params.id).then(img => {
+        const buf = Buffer.from(img.Body as Buffer);
+
         res.setHeader("Content-Type", img.ContentType);
+        res.setHeader("Content-Length", buf.length);
         return res.send(img.Body);
     }).catch(() => {
         return res.status(404).send("File Not Found");
