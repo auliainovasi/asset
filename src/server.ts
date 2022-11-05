e38be6f8a598c327c70d9de763919270caef4309
Aulia Rafi <62741404+rafi273@users.noreply.github.com>
2022-11-05 15:10:11 +0700

    add sentry

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,5 @@
+import Sentry from "@sentry/node";
+import Tracing from "@sentry/tracing";
 import errorHandler from "errorhandler";
 import app from "./app";
 import "dotenv/config";
@@ -8,6 +10,29 @@ import "dotenv/config";
  */
 if (process.env.NODE_ENV === "development") {
     app.use(errorHandler());
+} else {
+    Sentry.init({
+        dsn: "https://457606566ada4d258e694abe99caf58a@o1106728.ingest.sentry.io/4504105480552448",
+        integrations: [
+            // enable HTTP calls tracing
+            new Sentry.Integrations.Http({ tracing: true }),
+            // enable Express.js middleware tracing
+            new Tracing.Integrations.Express({ app }),
+        ],
+
+        // Set tracesSampleRate to 1.0 to capture 100%
+        // of transactions for performance monitoring.
+        // We recommend adjusting this value in production
+        tracesSampleRate: 1.0,
+    });
+
+    // RequestHandler creates a separate execution context using domains, so that every
+    // transaction/span/breadcrumb is attached to its own Hub instance
+    app.use(Sentry.Handlers.requestHandler());
+    // TracingHandler creates a trace for every incoming request
+    app.use(Sentry.Handlers.tracingHandler());
+    // The error handler must be before any other error middleware and after all controllers
+    app.use(Sentry.Handlers.errorHandler());
 }
 
 
