9ef6529abb9d0a7848305ba03aa4a307b04431dc
Aulia Rafi <62741404+rafi273@users.noreply.github.com>
2023-02-06 19:59:14 +0700

    fix env

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import "dotenv/config";
 /**
  * Error Handler. Provides full stack
  */
-if (process.env.NODE_ENV === "development") {
+if (process.env.NODE_ENV ?? app.get("env") === "development") {
     app.use(errorHandler());
 }
 
@@ -14,11 +14,11 @@ if (process.env.NODE_ENV === "development") {
 /**
  * Start Express server.
  */
-const server = app.listen(process.env.PORT, () => {
+const server = app.listen(process.env.PORT ?? app.get("port"), () => {
     console.log(
         "  App is running at http://localhost:%d in %s mode",
-        process.env.PORT,
-        process.env.NODE_ENV
+        process.env.PORT ?? app.get("port"),
+        process.env.NODE_ENV ?? app.get("env")
     );
     console.log("  Press CTRL-C to stop\n");
 });
